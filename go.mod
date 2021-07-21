aab05f85c8de2843936ff33a0ac2f67d5886c151
Trock <g_trock@163.com>
2021-07-21 15:29:52 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/DoNewsCode/core-processor
+
+go 1.16
+
+require (
+	github.com/DoNewsCode/core v0.7.3
+	github.com/go-kit/kit v0.11.0
+	github.com/oklog/run v1.1.0
+	github.com/pkg/errors v0.9.1
+	github.com/segmentio/kafka-go v0.4.17
+	github.com/stretchr/testify v1.7.0
+)
---
+++ setup_test.go
@@ -0,0 +1,95 @@
+package processor
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"os"
+	"strconv"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/DoNewsCode/core"
+	"github.com/DoNewsCode/core/otkafka"
+	"github.com/segmentio/kafka-go"
+)
+
+var envDefaultKafkaAddrs []string
+var testTopic = "processor-test"
+
+func TestMain(m *testing.M) {
+	v := os.Getenv("KAFKA_ADDR")
+	if v == "" {
+		fmt.Println("Set env KAFKA_ADDR to run processor tests")
+		os.Exit(0)
+	}
+	envDefaultKafkaAddrs = strings.Split(v, ",")
+	setupTopic()
+	setupMessage()
+
+	time.Sleep(1 * time.Second)
+
+	os.Exit(m.Run())
+}
+
+func setupTopic() {
+	conn, err := kafka.Dial("tcp", envDefaultKafkaAddrs[0])
+	if err != nil {
+		panic(err.Error())
+	}
+	defer conn.Close()
+
+	controller, err := conn.Controller()
+	if err != nil {
+		panic(err.Error())
+	}
+	var controllerConn *kafka.Conn
+	controllerConn, err = kafka.Dial("tcp", net.JoinHostPort(controller.Host, strconv.Itoa(controller.Port)))
+	if err != nil {
+		panic(err.Error())
+	}
+	defer controllerConn.Close()
+
+	topics := []string{testTopic}
+	topicConfigs := make([]kafka.TopicConfig, len(topics))
+	for i, topic := range topics {
+		topicConfigs[i] = kafka.TopicConfig{
+			Topic:             topic,
+			NumPartitions:     1,
+			ReplicationFactor: 1,
+		}
+	}
+	err = controllerConn.DeleteTopics(topics...)
+	if err == nil {
+		time.Sleep(2 * time.Second)
+	}
+
+	err = controllerConn.CreateTopics(topicConfigs...)
+
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+func setupMessage() {
+	c := core.New(
+		core.WithInline("kafka.writer.default.brokers", envDefaultKafkaAddrs),
+		core.WithInline("kafka.writer.default.topic", testTopic),
+		core.WithInline("log.level", "none"),
+	)
+	c.ProvideEssentials()
+	c.Provide(otkafka.Providers())
+
+	c.Invoke(func(w *kafka.Writer) {
+		testMessages := make([]kafka.Message, 4)
+		for i := 0; i < 4; i++ {
+			testMessages[i] = kafka.Message{Value: []byte(fmt.Sprintf(`{"id":%d}`, i))}
+		}
+		err := w.WriteMessages(context.Background(), testMessages...)
+		if err != nil {
+			panic(err)
+		}
+	})
+
+}
